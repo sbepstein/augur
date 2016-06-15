44eb2dea85c889fb9d207d7dc4dcd111bc286771
Stephen Sprinkle <git@sprinkle.io>
2016-06-15 09:34:31 -0700

    re-implemented test for submitNewMarket

--- test/create-market/actions/submit-new-market-test.js
+++ test/create-market/actions/submit-new-market-test.js
@@ -20,119 +20,119 @@ import {
 } from '../../../src/modules/transactions/constants/statuses';
 
 describe(`modules/create-market/actions/submit-new-market.js`, () => {
-	// proxyquire.noPreserveCache().noCallThru();
-	// const middlewares = [thunk];
-	// const mockStore = configureMockStore(middlewares);
-	// let store, action, out, clock;
-	// let state = Object.assign({}, testState);
-	// store = mockStore(state);
-	// let testData = {
-	// 	type: 'UPDATE_TRANSACTIONS_DATA',
-	// 	test123: {
-	// 		type: 'create_market',
-	// 		gas: 0,
-	// 		ether: 0,
-	// 		data: {
-	// 			market: 'some marketdata'
-	// 		},
-	// 		action: 'do some action',
-	// 		status: 'pending'
-	// 	}
-	// };
-	// let fakeInclude = {
-	// 	addCreateMarketTransaction: () => {}
-	// };
-    //
-	// sinon.stub(fakeInclude, 'addCreateMarketTransaction', (newMarket) => testData);
-    //
-	// let fakeAugurJS = {};
-	// fakeAugurJS.createMarket = sinon.stub().yields(null, {
-	// 	marketID: 'test123',
-	// 	status: SUCCESS,
-	// });
-	// fakeAugurJS.createMarket.onCall(1).yields({
-	// 	message: 'error!'
-	// }, {
-	// 	status: FAILED
-	// });
-    //
-	// // let fakeLoadMarket = {};
-    //
-	// let fakeGenerateOrderBook = {};
-    //
-	// action = proxyquire(
-	// 	'../../../src/modules/create-market/actions/submit-new-market',
-	// 	// {
-	// 	// 	'./generate-order-book': fakeGenerateOrderBook,
-	// 	// 	'../../transactions/actions/add-create-market-transaction': fakeInclude,
-	// 	// 	'../../../services/augurjs': fakeAugurJS,
-	// 	// 	'../../market/actions/load-market': fakeLoadMarket
-	// 	// }
-	// );
-    //
-	// fakeLoadMarket.loadMarket = sinon.stub().returns({
-	// 	type: 'loadMarket'
-	// });
-    //
-	// fakeGenerateOrderBook = sinon.stub().returns({
-	// 	type: 'TEST'
-	// });
-    //
-	// beforeEach(() => {
-	// 	store.clearActions();
-	// 	clock = sinon.useFakeTimers();
-	// 	// Mock the window object
-	// 	global.window = {};
-	// 	global.window.performance = {
-	// 		now: () => Date.now()
-	// 	};
-	// 	global.window.location = {
-	// 		pathname: '/test',
-	// 		search: 'example'
-	// 	};
-	// 	global.window.history = {
-	// 		state: [],
-	// 		pushState: (a, b, c) => window.history.state.push(c)
-	// 	};
-	// 	global.window.scrollTo = (x, y) => true;
-	// });
-    //
-	// afterEach(() => {
-	// 	global.window = {};
-	// 	store.clearActions();
-	// 	clock.restore();
-	// });
+	proxyquire.noPreserveCache().noCallThru();
+	const middlewares = [thunk];
+	const mockStore = configureMockStore(middlewares);
+	let store, action, out, clock;
+	let state = Object.assign({}, testState);
+	store = mockStore(state);
+	let testData = {
+		type: 'UPDATE_TRANSACTIONS_DATA',
+		test123: {
+			type: 'create_market',
+			gas: 0,
+			ether: 0,
+			data: {
+				market: 'some marketdata'
+			},
+			action: 'do some action',
+			status: 'pending'
+		}
+	};
+	let stubbedNewMarketTransactions = {
+		addCreateMarketTransaction: () => {}
+	};
+	sinon.stub(stubbedNewMarketTransactions, 'addCreateMarketTransaction', (newMarket) => testData);
+    
+	let fakeAugurJS = {};
+	fakeAugurJS.createMarket = sinon.stub().yields(null, {
+		marketID: 'test123',
+		status: SUCCESS,
+	});
+	fakeAugurJS.createMarket.onCall(1).yields({
+		message: 'error!'
+	}, {
+		status: FAILED
+	});
+    
+	let fakeLoadMarket = {
+		loadMarket: () => {}
+	};
+	fakeLoadMarket.loadMarket = sinon.stub().returns({
+		type: 'loadMarket'
+	});
+    
+	let fakeGenerateOrderBook = {
+		
+	};
+	fakeGenerateOrderBook = sinon.stub().returns({
+		type: 'TEST'
+	});
+    
+	action = proxyquire(
+		'../../../src/modules/create-market/actions/submit-new-market',
+		{
+			'./generate-order-book': fakeGenerateOrderBook,
+			'../../transactions/actions/add-create-market-transaction': stubbedNewMarketTransactions,
+			'../../../services/augurjs': fakeAugurJS,
+			'../../market/actions/load-market': fakeLoadMarket
+		}
+	);
+    
+	beforeEach(() => {
+		store.clearActions();
+		clock = sinon.useFakeTimers();
+		// Mock the window object
+		global.window = {};
+		global.window.performance = {
+			now: () => Date.now()
+		};
+		global.window.location = {
+			pathname: '/test',
+			search: 'example'
+		};
+		global.window.history = {
+			state: [],
+			pushState: (a, b, c) => window.history.state.push(c)
+		};
+		global.window.scrollTo = (x, y) => true;
+	});
+    
+	afterEach(() => {
+		global.window = {};
+		store.clearActions();
+		clock.restore();
+	});
 
 	it(`should be able to submit a new market`, () => {
-
-		// store.dispatch(action.submitNewMarket({
-		// 	market: {
-		// 		id: 'market'
-		// 	}
-		// }));
-		// out = [{
-		// 	type: 'SHOW_LINK',
-		// 	parsedURL: {
-		// 		pathArray: ['/transactions'],
-		// 		searchParams: {},
-		// 		url: '/transactions'
-		// 	}
-		// }, {
-		// 	type: 'UPDATE_TRANSACTIONS_DATA',
-		// 	'test123': {
-		// 		type: 'create_market',
-		// 		gas: 0,
-		// 		ether: 0,
-		// 		data: {
-		// 			market: 'some marketdata'
-		// 		},
-		// 		action: 'do some action',
-		// 		status: 'pending'
-		// 	}
-		// }];
-        //
-		// assert(fakeInclude.addCreateMarketTransaction.calledOnce, `addCreateMarketTransaction wasn't called once as expected`);
-		// assert.deepEqual(store.getActions(), out, `Didn't correctly create a new market`);
+		store.dispatch(action.submitNewMarket({
+			market: {
+				id: 'market'
+			}
+		}));
+		out = [{
+			type: 'SHOW_LINK',
+			parsedURL: {
+				pathArray: ['/transactions'],
+				searchParams: {},
+				url: '/transactions'
+			}
+		}, {
+			type: 'UPDATE_TRANSACTIONS_DATA',
+			'test123': {
+				type: 'create_market',
+				gas: 0,
+				ether: 0,
+				data: {
+					market: 'some marketdata'
+				},
+				action: 'do some action',
+				status: 'pending'
+			}
+		}];
+        
+		assert(stubbedNewMarketTransactions.addCreateMarketTransaction.calledOnce, `addCreateMarketTransaction wasn't called once as expected`);
+		assert.deepEqual(store.getActions(), out, `Didn't correctly create a new market`);
 	});
 
 	it(`should be able to create a new market`, () => {
@@ -171,6 +171,4 @@ describe(`modules/create-market/actions/submit-new-market.js`, () => {
 		// assert(fakeAugurJS.createMarket.calledTwice, `createMarket wasn't called twice after dispatching a createMarket Action 2 times`);
 		// assert.deepEqual(store.getActions(), [], `Didn't properly dispatch actions for a error when creating account`);
 	});
-
-	it('[TODO] should be able to generate an order book');
 });
